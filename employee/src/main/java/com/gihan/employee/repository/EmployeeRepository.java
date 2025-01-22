01d356c99f7fc6fff70d32965d66679cb4b2af8b
Gihan_Sachith <gihansachith92@gmail.com>
2025-01-22 12:07:05 +0530

    repository and service layer

---
+++ employee/src/main/java/com/gihan/employee/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.gihan.employee.repository;
+
+import com.gihan.employee.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
---
+++ employee/src/main/java/com/gihan/employee/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.gihan.employee.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+}
